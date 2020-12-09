0dffccef3c289a373d6a6c06dd328b7955cd3109
Red <redsanchezz@yandex.ru>
2020-12-09 23:59:52 +0400

    header

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,51 @@
 var gulp = require('gulp');
 let browserSync = require('browser-sync');
-let sass = require('gulp-sass');
-
-
-
+let scss = require('gulp-sass');
+let concat = require('gulp-concat');
 
 gulp.task("watch", function() {
-    gulp.watch('./app/scss/**/*.scss', gulp.parallel("scss"));
-    gulp.watch('./app/*.html', gulp.parallel("html"));
+    gulp.watch('./app/*.html', gulp.parallel('html'));
+    gulp.watch('./app/scss/**/*.scss', gulp.parallel('scss'));
+    gulp.watch('./app/js/**/*.js', gulp.parallel('js'));
 });
 
+gulp.task('html', function() {
+    return gulp.src('app/*.html').pipe(browserSync.reload({ stream: true }));
+});
 
-gulp.task("scss", function() {
-    return gulp.src('./app/scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./app/css'))
+gulp.task('scss', function() {
+    return gulp.src("app/scss/**/*.scss")
+        .pipe(scss().on('error', scss.logError))
+        .pipe(gulp.dest('app/css/'))
         .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task("js", function() {
-    return gulp.src('./app/js/*.js').pipe(gulp.dest('./dest/js'));
+
+gulp.task("css", function() {
+    return gulp.src("./node_modules/normalize.css/normalize.css").pipe();
 });
 
-gulp.task("html", function() {
-    return gulp.src("app/*.html").pipe(browserSync.reload({ stream: true }));
+
+gulp.task('js', function() {
+    return gulp.src("app/js/**/*.js")
+        .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('bs', function() {
+
+gulp.task("bs", function() {
     browserSync.init({
         server: {
-            baseDir: "./app/"
+            baseDir: "app/"
         }
-    })
+    });
 });
-gulp.task("default", gulp.parallel('bs', 'watch'));
+
+gulp.task("lib", function() {
+    gulp.src(["./node_modules/jquery/dist/jquery.min.js", "./node_modules/slick-carousel/slick/slick.min.js"])
+        .pipe(concat("lib.js"))
+        .pipe(gulp.dest("./app/js/libs"));
+    gulp.src(["./node_modules/slick-carousel/slick/slick.css", "./node_modules/normalize.css/normalize.css"])
+        .pipe(concat("libs.css"))
+        .pipe(gulp.dest("./app/css/libs"));
+});
+gulp.task("default", gulp.parallel('bs', 'scss', 'watch'));
