54c81a05f48d3478f1d4f07689610de2f3ee540d
Red <redsanchezz@yandex.ru>
2020-12-11 12:07:52 +0400

    end_layout

--- app/js/script.js
+++ app/js/script.js
@@ -13,41 +13,61 @@ $(".work__slider").slick({
     prevArrow: ".work__slider-left-arrow",
     nextArrow: ".work__slider-right-arrow"
 });
-let itemArr = document.querySelectorAll(".skills-block__item");
-setActive(itemArr[0]);
 
-for (const item of itemArr) {
-    let block = item.querySelector(".skills-block__text");
-    let header = item.querySelector(".skills-block__header");
-    header.addEventListener("click", function(e) {
-        if (block.classList.contains("hide__text")) {
-            setUnActiveAll(itemArr);
-            setActive(item);
-        } else {
-            setUnActive(item);
-        }
-    })
+{
+    let showBtn = document.querySelector(".map__center-block");
+    showBtn.addEventListener("click", function() {
+        let googlePopup = document.querySelector(".google-map__popup");
+        googlePopup.style.display = "flex";
+        googlePopup.style.height = "100%";
+        document.querySelector("body").style.overflow = "hidden";
+    });
+    let exitBtn = document.querySelector(".google-map__exit");
+    exitBtn.addEventListener("click", function(e) {
+        let googlePopup = document.querySelector(".google-map__popup");
+        // googlePopup.style.display = "none";
+        googlePopup.style.height = "0%";
+        document.querySelector("body").style.overflow = "";
+    });
+
 }
 
-function setUnActiveAll(items) {
-    for (const iterator of items) {
-        setUnActive(iterator);
+{
+    let itemArr = document.querySelectorAll(".skills-block__item");
+    setActive(itemArr[0]);
+
+    for (const item of itemArr) {
+        let block = item.querySelector(".skills-block__text");
+        let header = item.querySelector(".skills-block__header");
+        header.addEventListener("click", function(e) {
+            if (block.classList.contains("hide__text")) {
+                setUnActiveAll(itemArr);
+                setActive(item);
+            } else {
+                setUnActive(item);
+            }
+        })
     }
-}
 
-function setActive(item) {
-    let header = item.querySelector(".skills-block__header");
-    let block = item.querySelector(".skills-block__text");
-    let arrow = item.querySelector(".skills-block__arrow");
-    block.classList.remove("hide__text");
-    arrow.style.transform = "rotateX(0deg)";
-}
+    function setUnActiveAll(items) {
+        for (const iterator of items) {
+            setUnActive(iterator);
+        }
+    }
 
-function setUnActive(item) {
-    let header = item.querySelector(".skills-block__header");
-    let block = item.querySelector(".skills-block__text");
-    let arrow = item.querySelector(".skills-block__arrow");
-    block.classList.add("hide__text");
-    arrow.style.transform = "rotateX(180deg)";
+    function setActive(item) {
+        let header = item.querySelector(".skills-block__header");
+        let block = item.querySelector(".skills-block__text");
+        let arrow = item.querySelector(".skills-block__arrow");
+        block.classList.remove("hide__text");
+        arrow.style.transform = "rotateX(0deg)";
+    }
 
+    function setUnActive(item) {
+        let header = item.querySelector(".skills-block__header");
+        let block = item.querySelector(".skills-block__text");
+        let arrow = item.querySelector(".skills-block__arrow");
+        block.classList.add("hide__text");
+        arrow.style.transform = "rotateX(180deg)";
+    }
 }
