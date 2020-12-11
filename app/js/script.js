12928186160884e0e90ce7e3e70b1ae36779f01e
Red <redsanchezz@yandex.ru>
2020-12-11 04:42:22 +0400

    presents-block

---
+++ app/js/script.js
@@ -0,0 +1,49 @@
+$(".skills-block__text").mCustomScrollbar({
+    theme: "my-theme",
+    autoDraggerLength: true,
+    scrollbarPosition: "outside"
+
+});
+
+$(".presents__slider").slick({
+    prevArrow: ".presents__slider-left-arrow",
+    nextArrow: ".presents__slider-right-arrow"
+});
+let itemArr = document.querySelectorAll(".skills-block__item");
+setActive(itemArr[0]);
+
+for (const item of itemArr) {
+    let block = item.querySelector(".skills-block__text");
+    let header = item.querySelector(".skills-block__header");
+    header.addEventListener("click", function(e) {
+        if (block.classList.contains("hide__text")) {
+            setUnActiveAll(itemArr);
+            setActive(item);
+        } else {
+            setUnActive(item);
+        }
+    })
+}
+
+function setUnActiveAll(items) {
+    for (const iterator of items) {
+        iterator.querySelector(".skills-block__text").classList.add("hide__text");
+    }
+}
+
+function setActive(item) {
+    let header = item.querySelector(".skills-block__header");
+    let block = item.querySelector(".skills-block__text");
+    let arrow = item.querySelector(".skills-block__arrow");
+    block.classList.remove("hide__text");
+    arrow.style.transform = "rotateX(0deg)";
+}
+
+function setUnActive(item) {
+    let header = item.querySelector(".skills-block__header");
+    let block = item.querySelector(".skills-block__text");
+    let arrow = item.querySelector(".skills-block__arrow");
+    block.classList.add("hide__text");
+    arrow.style.transform = "rotateX(180deg)";
+
+}
